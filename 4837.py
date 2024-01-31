283e81992cdce2437b3625fa2f77a39577d95fb7
Eunkyong Choi <eunkyyong@gmail.com>
2024-01-31 17:52:23 +0900

    should review

--- 4837.py
+++ 4837.py
@@ -1,22 +1,24 @@
-arr = []
-def f(arr, N): # 모든 부분집합 구하기
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+def f(arr): # 모든 부분집합 구하기
     # i가 0인 경우 공집합
-    for i in range(1<<N): # 1<<N : 8==2**3 ==1000 (1<<3) binary.
-        s = 0
-        for j in range(N):
-            if i & (1<<j):
+    ans = 0
+    for i in range(1<<12): # 1<<N : 8==2**3 ==1000 (1<<3) binary.
+        s = 0 # 부분집합 원소의 합
+        cnt = 0 # 부분집합의 개수
+        for j in range(12):
+            t = i & (1<<j) # i는 (0 ~ 1<<N)
+            if t:
                 s += arr[j]
-                # print(arr[j], end=" ")
-        # print(s)
-        if s == 0:
-            return True
-    return False
-def subSet():
+                cnt += 1
+
+        if s == K and cnt == N:
+            ans += 1
+    return ans
+
+# def subSet():
     # 위 부분집합 중 N개의 원소를 갖고, 원소의 합이 K인 부분집합의 개수
-
-
 T = int(input())
 for num in range(T):
     N, K = list(map(int, input().split()))
-    a = f(arr, N)
-    print(f'#{i + 1} {a}')
+    a = f(arr)
+    print(f'#{num + 1} {a}')
