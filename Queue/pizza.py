53c97436d227323c45e2aaa1f91f034ae8f36dc6
Eunkyong Choi <eunkyyong@gmail.com>
2024-02-15 17:55:01 +0900

    exam..

--- Queue/pizza.py
+++ Queue/pizza.py
@@ -7,23 +7,18 @@ for tc in range(1, T+1):
     fp = [0] * N
     nextPizzaNo = 1
     while fp:
-        # pizzaNo = numb.pop[0]
-        # pizzaNo == 1일 때, 치즈양?
-        # 치즈가 0이면 피자 다시 넣지 않고 새 피자 넣기
-        if cheese == 0:
-            if Ci:  # 남은 피자가 있을 때 화덕에 남은 피자 넣기
-                piz = Ci.pop(0)
-                fp.append(piz)
-                numb[i] = piz
-            else:  # 남은 피자 없을 때
-                numb[i] =
-                continue
-        # fp에 남은 피자가 하나고 치즈 양이 0일 때 break
-        if len(fp) ==1 and cheese == 0:
-            break
+        pizzaNo = fp.pop(0)
+        Ci[pizzaNo] //= 2
+        if Ci[pizzaNo] == 0:
+        # 남은 피자가 있을 때 화덕에 남은 피자 넣기
+        # Next pizza 하고 m하고의 관계
+            if nextPizzaNo <= M:
+                fp.append(nextPizzaNo)
+                nextPizzaNo += 1
+        else:
+            fp.append(pizzaNo)
 
-    print(f'#{tc}', end = ' ')
-    print(fp[0])
+    print(f'#{tc} {pizzaNo}')
 
 
 
