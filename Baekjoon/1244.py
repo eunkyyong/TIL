ce665a56980167b8ed945881fad2849a7b68ece9
eunkyong <eunkyyong@gmail.com>
2024-02-18 17:47:59 +0900

    .

--- Baekjoon/1244.py
+++ Baekjoon/1244.py
@@ -1,15 +1,22 @@
 def m_switch(b):
-    for num in range(1, N+1):  # 번호는 1부터 시작되니까
+    for num in range(1, len(st)):
         if num % b == 0:
             st[num] = (st[num]+1)%2
 
 def f_switch(a):
-    # 여학생이 받은 숫자를 기준으로 스위치 상태 바꾸기
     st[a] = int(not st[a])
-    # left right independantly
-
-
-
+    for lf in range(1, a, -1):
+        for rg in range(a+1, len[st]):
+            if st[lf] == st[rg]:
+                st[lf] = int(not st[lf])
+                st[rg] = int(not st[rg])
+    # for i in range(0, a):
+    #     lf = a-i
+    #     # rg in range(a, N)
+    #     rg = a+i
+    #     if st[lf] == st[rg]:
+    #         lf = int(not lf)
+    #         rg = int(not rg)
 
 N = int(input())
 st = [0] + list(map(int, input().split()))
@@ -20,4 +27,5 @@ for _ in range(st_no):
         m_switch(card_no)
     elif sex == 2:
         f_switch(card_no)
+st.pop(0)
 print(*st)
