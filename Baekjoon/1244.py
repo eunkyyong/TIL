2045da4995dcc28f8098410d03f73229450bd176
eunkyong <eunkyyong@gmail.com>
2024-02-18 22:03:18 +0900

    practice while

--- Baekjoon/1244.py
+++ Baekjoon/1244.py
@@ -1,22 +1,19 @@
-def m_switch(b):
-    for num in range(1, len(st)):
+def m_switch(b, C):
+    for num in range(1, len(C)):
         if num % b == 0:
-            st[num] = (st[num]+1)%2
+            C[num] = (C[num]+1) % 2
 
-def f_switch(a):
-    st[a] = int(not st[a])
-    for lf in range(1, a, -1):
-        for rg in range(a+1, len[st]):
-            if st[lf] == st[rg]:
-                st[lf] = int(not st[lf])
-                st[rg] = int(not st[rg])
-    # for i in range(0, a):
-    #     lf = a-i
-    #     # rg in range(a, N)
-    #     rg = a+i
-    #     if st[lf] == st[rg]:
-    #         lf = int(not lf)
-    #         rg = int(not rg)
+
+def f_switch(a, D):
+    D[a] = 1-D[a]
+    lf = a - 1
+    rg = a + 1
+    while lf >= 1 and rg <= len(D) and D[lf] == D[rg]:
+        D[lf] = 1-D[lf]
+        D[rg] = 1-D[rg]
+        lf -= 1
+        rg += 1
+
 
 N = int(input())
 st = [0] + list(map(int, input().split()))
@@ -24,8 +21,10 @@ st_no = int(input())
 for _ in range(st_no):
     sex, card_no = map(int, input().split())
     if sex == 1:
-        m_switch(card_no)
+        m_switch(card_no, st)
     elif sex == 2:
-        f_switch(card_no)
+        f_switch(card_no, st)
 st.pop(0)
-print(*st)
+for t in range(0, N//20+1):
+    string = st[20*t:min(20*(t+1), N)]
+    print(*string)
