50d680ceb5728a21323fa094531e5f6cb3864d13
eunkyong <eunkyyong@gmail.com>
2024-02-22 01:22:50 +0900

    ask

--- Forexam/Russian.py
+++ Forexam/Russian.py
@@ -17,6 +17,7 @@ T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     arr = [list(input()) for _ in range(N)]
+    print(arr)
 
     narr = [[0]*3 for _ in range(N)]
     for o in range(0, N):
@@ -24,19 +25,19 @@ for tc in range(1, T+1):
         narr[o][1] = arr[o].count('B')
         narr[o][2] = arr[o].count('R')
     print(narr)
-    Sum = 0
+
     Min = 1000
-    for l1 in range(1, N-2):
-        for l2 in range(2, N-1):
-            if l1 < l2:
+    for l1 in range(1, N-1):
+        for l2 in range(2, N):
+            if l1<l2:
+                Sum = 0
                 for i in range(0, l1):
                     Sum += narr[i][1] + narr[i][2]
                 for j in range(l1, l2):
                     Sum += narr[j][0] + narr[j][2]
                 for k in range(l2, N):
                     Sum += narr[k][0] + narr[k][1]
-            print(Sum)
-            if Min > Sum:
-                Min = Sum
-                Sum = 0
+
+                if Min > Sum:
+                    Min = Sum
     print(f'#{tc} {Min}')
