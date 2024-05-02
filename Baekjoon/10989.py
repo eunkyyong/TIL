98403c51b92f26e6c6d80963cf24a750a22137b2
Anne Choi <eunkyyong@gmail.com>
2024-05-02 17:43:21 +0900

    not yet

---
+++ Baekjoon/10989.py
@@ -0,0 +1,19 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+N = int(input())
+lst = []
+for _ in range(N):
+    num = int(input())
+    lst.append(num)
+
+asc_lst = insertion_sort(lst)
+for i in asc_lst:
+    print(i)
